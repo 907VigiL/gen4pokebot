87d96522a4d86ecac26600605eae7cad947c2a09
907VigiL <94725239+907VigiL@users.noreply.github.com>
2023-08-30 08:58:45 -0800

    Add files via upload

---
+++ lua/methods_gen_iv.lua
@@ -0,0 +1,223 @@
+function update_pointers()
+    offset.party_count = mdword(0x021C489C) + 14
+    offset.party_data = offset.party_count + 4
+
+    offset.foe_count = mdword(0x21C5A08) + 0x729C
+    offset.current_foe = offset.foe_count + 4
+
+    offset.map_header = mdword(0x21C489C) + 0x11B2
+    offset.trainer_x = offset.map_header + 4 + 2
+    offset.trainer_y = offset.map_header + 12 + 2
+    offset.trainer_z = offset.map_header + 8 + 2
+    -- console.log()
+    -- offset.trainer_x =
+
+    -- console.log(string.format("%08X", offset.map_header))
+end
+
+function flee_battle()
+    while game_state.in_battle do
+        touch_screen_at(125, 175) -- Run
+        wait_frames(20)
+    end
+end
+
+function catch_pokemon()
+    if config.auto_catch then
+        pause_bot("Wild Pokemon meets target specs!")
+    else
+        console.log("Attempting to catch pokemon now...")
+        wait_frames(900)
+        touch_screen_at(40, 170)
+        wait_frames(50)
+        touch_screen_at(190, 45)
+        wait_frames(20)
+        touch_screen_at(60, 30)
+        wait_frames(20)
+        touch_screen_at(100, 170)
+        wait_frames(750)
+        if mbyte(0x02101DF0) == 0x01 then
+            console.log("Pokemon caught!!!")
+            skip_nickname()
+        else
+            console.log("Faile catch trying again...")
+            catch_pokemon()
+        end
+    end
+end
+
+function skip_nickname()
+    while game_state.in_battle do
+        touch_screen_at(125, 140)
+        wait_frames(20)
+    end
+end
+
+function process_wild_encounter()
+    -- Check all foes in case of a double battle in Eterna Forest
+    local foe_is_target = false
+    for i = 1, #foe, 1 do
+        foe_is_target = pokemon.log(foe[i]) or foe_is_target
+    end
+
+    wait_frames(30)
+
+    if foe_is_target then
+        console.log("Wild " .. foe[1].name .. " was a target!!! Catching Now")
+        catch_pokemon()
+    else
+        console.log("Wild " .. foe[1].name .. " was not a target, fleeing!")
+
+        flee_battle()
+    end
+end
+
+-----------------------
+-- BOT MODES
+-----------------------
+
+function mode_starters_DP(starter)
+    if not game_state.in_game then
+        console.log("Waiting to reach overworld...")
+
+        while not game_state.in_game do
+            skip_dialogue()
+        end
+    end
+
+    hold_button("Up") -- Enter Lake Verity
+    console.log("Waiting to reach briefcase...")
+
+    -- Skip through dialogue until starter select
+    while not (mdword(offset.starters_ready) > 0) do
+        skip_dialogue()
+    end
+
+    release_button("Up")
+
+    -- Highlight and select target
+    console.log("Selecting starter...")
+
+    while mdword(offset.selected_starter) < starter do
+        press_sequence("Right", 5)
+    end
+
+    while #party == 0 do
+        press_sequence("A", 6)
+    end
+
+    if not config.hax then
+        console.log("Waiting to see starter...")
+
+        for i = 0, 86, 1 do
+            press_button("A")
+            clear_unheld_inputs()
+            wait_frames(6)
+        end
+    end
+
+    mon = party[1]
+    local was_target = pokemon.log(mon)
+
+    if was_target then
+        pause_bot("Starter meets target specs!")
+    else
+        console.log("Starter was not a target, resetting...")
+        press_button("Power")
+        wait_frames(180)
+    end
+end
+
+function mode_starters(starter)
+    if not game_state.in_game then
+        console.log("Waiting to reach overworld...")
+
+        while mbyte(offset.battle_indicator) ~= 0xFF do
+            skip_dialogue()
+            if mbyte(offset.battle_indicator) == 0xFF then
+                break
+            end
+        end
+    end --]]
+    --we can save right in front of the bag in platinum so all we have to do is open and select are starter
+
+    -- Open briefcase and skip through dialogue until starter select
+    console.log("Skipping dialogue to briefcase")
+
+    for j = 0, 21, 1 do
+        skip_dialogue()
+    end
+    wait_frames(120)
+    console.log("Starting to find offsets")
+    for i = 0x022BF900, 0x022BFA9E, 0x02 do
+        --console.log("i = " .. i)
+        --console.log("word at i = " .. mword(i))
+        if mword(i) == 0xA94D then
+            offset.starters_ready = i
+            console.log("starter ready value: " .. offset.starters_ready)
+            break
+        end
+    end
+
+    offset.selected_starter = offset.starters_ready - 0x84
+    console.log("selected_starter: " .. offset.selected_starter)
+
+    -- Need to wait for hand to be visible to find offset
+    console.log("Selecting starter...")
+
+    -- Highlight and select target
+    while mdword(offset.selected_starter) < starter do
+        press_sequence("Right", 10)
+    end
+
+    while #party == 0 do
+        press_sequence("A", 6)
+    end
+
+    if not config.hax then
+        console.log("Waiting to see starter...")
+
+        for i = 0, 86, 1 do
+            press_button("A")
+            clear_unheld_inputs()
+            wait_frames(6)
+        end
+    end
+
+    mon = party[1]
+    local was_target = pokemon.log(mon)
+
+    if was_target then
+        pause_bot("Starter meets target specs!")
+    else
+        console.log("Starter was not a target, resetting...")
+        press_button("Power")
+        wait_frames(180)
+    end
+end
+
+function mode_random_encounters()
+    console.log("Attempting to start a battle...")
+
+    local tile_frames = frames_per_move() - 2
+    local dir1 = config.move_direction == "Horizontal" and "Left" or "Up"
+    local dir2 = config.move_direction == "Horizontal" and "Right" or "Down"
+
+    while not foe and not game_state.in_battle do
+        hold_button("B")
+        hold_button(dir1)
+        wait_frames(tile_frames)
+        release_button(dir1)
+        release_button("B")
+
+        hold_button("B")
+        hold_button(dir2)
+        wait_frames(tile_frames)
+        release_button(dir2)
+        release_button("B")
+    end
+
+    release_button(dir2)
+
+    process_wild_encounter()
+end
---
+++ pokebot-nds.lua
@@ -0,0 +1,338 @@
+-----------------------
+-- INITIALIZATION
+-----------------------
+local BOT_VERSION = "0.4.0-alpha"
+
+console.clear()
+console.log("Running " .. _VERSION)
+console.log("Pokebot NDS version " .. BOT_VERSION .. " by NIDO (wyanido)")
+
+mbyte = memory.read_u8
+mword = memory.read_u16_le
+mdword = memory.read_u32_le
+console.debug = function(message)
+    if config.debug then
+        console.log("- " .. message)
+    end
+end
+console.warning = function(message)
+    console.log("# " .. message .. " #")
+end
+
+-- Requirements
+json = require("lua\\json")
+dofile("lua\\input.lua")
+dofile("lua\\game_setup.lua")
+
+pokemon = require("lua\\pokemon")
+
+dofile("lua\\dashboard.lua")
+
+-----------------------
+-- GAME INFO POLLING
+-----------------------
+
+-- Send game info to the dashboard
+comm.socketServerSend(json.encode({
+    type = "init",
+    data = {
+        gen = gen,
+        game = game_name
+    }
+}) .. "\x00")
+
+last_party_checksums = {}
+party = {}
+
+function get_party(force)
+    -- Prevent reading out of bounds in gen IV games
+    -- by pretending that the party is empty
+    if offset.party_count < 0x02000000 then
+        party_changed = true
+        last_party_checksums = {}
+        party = {}
+        return true
+    end
+
+    local party_size = mbyte(offset.party_count)
+
+    -- Get the checksums of all party members
+    local checksums = {}
+    for i = 0, party_size - 1 do
+        table.insert(checksums, mword(offset.party_data + i * MON_DATA_SIZE + 0x06))
+    end
+
+    -- Check for changes in the party data
+    -- Necessary for only sending data to the socket when things have changed
+    if party_size == #party and not force then
+        local party_changed = false
+
+        for i = 1, #checksums, 1 do
+            if checksums[i] ~= last_party_checksums[i] then
+                party_changed = true
+                break
+            end
+        end
+
+        if not party_changed then
+            return false
+        end
+    end
+
+    -- Party changed, update info
+    console.debug("Party updated")
+    last_party_checksums = checksums
+    local new_party = {}
+
+    for i = 1, party_size do
+        local mon = pokemon.read_data(offset.party_data + (i - 1) * MON_DATA_SIZE)
+
+        if mon then
+            mon = pokemon.enrich_data(mon)
+
+            -- Friendship is used to track egg cycles
+            -- Converts cycles to steps
+            if mon.isEgg == 1 then
+                mon.friendship = mon.friendship * 256
+                mon.friendship = math.max(0,
+                    mon.friendship - mbyte(offset.step_counter) - mbyte(offset.step_cycle) * 256)
+            end
+
+            table.insert(new_party, mon)
+        else
+            -- If any party checksums fail, wait a frame and try again
+            console.debug("Party checksum failed at slot " .. i .. ", retrying")
+            emu.frameadvance()
+            return get_party(true)
+        end
+    end
+
+    party = new_party
+
+    return true
+end
+
+function get_current_foes()
+    -- Make sure it's not reading garbage non-battle data
+    if mbyte(offset.battle_indicator) ~= 0x41 or mbyte(offset.foe_count) == 0 then
+        foe = nil
+    elseif not foe then -- Only update foe on battle start
+        ::retry::
+        local foe_table = {}
+        local foe_count = mbyte(offset.foe_count)
+
+        for i = 1, foe_count do
+            local mon = pokemon.read_data(offset.current_foe + (i - 1) * MON_DATA_SIZE)
+
+            if mon then
+                mon = pokemon.enrich_data(mon)
+
+                table.insert(foe_table, mon)
+            else
+                console.debug("Foe checksum failed at slot " .. i .. ", retrying")
+                emu.frameadvance()
+                goto retry
+            end
+        end
+
+        foe = foe_table
+    end
+end
+
+function get_game_state()
+    local map = mword(offset.map_header)
+
+    -- Update in-game values
+    if gen == 4 then -- gen 4 is always considered "in game" even before the title screen, so it always returns real data
+        return {
+            map_header = map,
+            map_name = map_names[map + 1],
+            trainer_x = mword(offset.trainer_x + 2),
+            trainer_y = to_signed(mword(offset.trainer_y + 2)),
+            trainer_z = mword(offset.trainer_z + 2),
+            in_battle = mbyte(offset.battle_indicator) == 0x41 and mbyte(offset.foe_count) > 0
+        }
+    else
+        local in_game = (map ~= 0x0 and map <= MAP_HEADER_COUNT)
+        if in_game then
+            return {
+                -- map_matrix = mdword(offset.map_matrix),
+                map_header = map,
+                map_name = map_names[map + 1],
+                trainer_x = mword(offset.trainer_x + 2),
+                trainer_y = to_signed(mword(offset.trainer_y + 2)),
+                trainer_z = mword(offset.trainer_z + 2),
+                phenomenon_x = mword(offset.phenomenon_x + 2),
+                phenomenon_z = mword(offset.phenomenon_z + 2),
+                -- trainer_dir = mdword(offset.trainer_direction),
+                in_battle = mbyte(offset.battle_indicator) == 0x41 and mbyte(offset.foe_count) > 0,
+                in_game = true
+            }
+        else
+            -- Set minimum required values for the dashboard
+            return {
+                map_header = 0,
+                map_name = "--",
+                trainer_x = 0,
+                trainer_y = 0,
+                trainer_z = 0,
+                phenomenon_x = 0,
+                phenomenon_z = 0,
+                in_game = false
+            }
+        end
+    end
+end
+
+function frames_per_move()
+    if gen == 4 then -- Temporary
+        return 16
+    end
+
+    if mbyte(offset.on_bike) == 1 then
+        return 4
+    elseif mbyte(offset.running_shoes) > 0 then
+        return 8
+    end
+
+    return 16
+end
+
+function update_game_info(force)
+    -- Refresh data at the rate it takes to move 1 tile
+    local refresh_frames = frames_per_move() / 2
+
+    if emu.framecount() % refresh_frames == 0 or force then
+        game_state = get_game_state()
+        comm.socketServerSend(json.encode({
+            type = "game",
+            data = game_state
+        }) .. "\x00")
+
+        get_current_foes()
+    end
+
+    local party_changed = get_party()
+    if party_changed then
+        comm.socketServerSend(json.encode({
+            type = "party",
+            data = party
+        }) .. "\x00")
+    end
+end
+
+function pause_bot(reason)
+    clear_all_inputs()
+    client.clearautohold()
+
+    console.log("###################################")
+    console.log(reason .. ". Pausing bot! (Make sure to disable the lua script before intervening)")
+
+    -- Do nothing ever again
+    while true do
+        emu.frameadvance()
+    end
+end
+
+function process_frame()
+    emu.frameadvance()
+    update_pointers()
+    poll_dashboard_response()
+    update_game_info()
+end
+
+-----------------------
+-- PREPARATION
+-----------------------
+console.log("Waiting for dashboard to relay bot configuration...")
+::poll_config::
+
+emu.frameadvance()
+poll_dashboard_response()
+
+if config == nil then
+    goto poll_config
+end
+
+-----------------------
+-- MAIN BOT LOGIC
+-----------------------
+
+input = input_init()
+foe = nil -- Prevents logging old Pokemon when re-enabling the script in a different battle than the one it was disabled in
+update_pointers()
+update_game_info(true)
+
+::begin::
+clear_all_inputs()
+client.clearautohold()
+
+console.log("Bot mode set to " .. config.mode)
+mode_real = config.mode
+
+if config.save_game_on_start then
+    save_game()
+end
+
+local mode = string.lower(config.mode)
+local mode_function = _G["mode_" .. mode] -- Get the respective global scope function for the current bot mode
+local starter = -1
+
+while true do
+    if mode_function then
+        if mode == "starters" then
+            -- Alternate between starters specified in config and reset until one is a target
+            if not config.starter0 and not config.starter1 and not config.starter2 then
+                console.warning("At least one starter selection must be enabled in config for this bot mode")
+                return
+            end
+
+            -- Cycle to next enabled starter
+            starter = (starter + 1) % 3
+
+            while not config["starter" .. tostring(starter)] do
+                starter = (starter + 1) % 3
+            end
+            mode_starters(starter)
+        else
+            mode_function()
+        end
+    else
+        if mode == "manual" then -- No bot logic, just manual gameplay with a dashboard
+            while true do
+                while not game_state.in_battle do
+                    process_frame()
+                    -- Restart if config changed
+                    if mode_real ~= config.mode then
+                        goto begin
+                    end
+                end
+
+                for i = 1, #foe, 1 do
+                    pokemon.log(foe[i])
+                end
+
+                while game_state.in_battle do
+                    process_frame()
+
+                    -- Restart if config changed
+                    if mode_real ~= config.mode then
+                        goto begin
+                    end
+                end
+            end
+        else
+            console.log("Unknown bot mode: " .. config.mode)
+            return
+        end
+    end
+
+    -- Restart if config changed
+    if mode_real ~= config.mode then
+        goto begin
+    end
+
+    joypad.set(input)
+    process_frame()
+    clear_unheld_inputs()
+end
